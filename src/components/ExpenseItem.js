05f0494747db0303769939e4d9ac8f87c9ca9736
Mehmet Semerci <mehmet.semerci@yahoo.com>
2021-07-04 22:12:01 +0300

    Vscode commit

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,1 +1,15 @@
-import './ExpenseItem.css'
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <div className="expense-item__date">{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">₺{props.amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
