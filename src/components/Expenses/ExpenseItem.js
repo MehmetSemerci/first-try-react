593e9e2cf3b7361f64cbdccfbfaa38a1fdd9cb4a
Mehmet Semerci <mehmet.semerci@yahoo.com>
2021-07-11 20:05:27 +0300

    child to parent

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
@@ -6,22 +5,14 @@ import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
-  
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle('Heelo');
-    console.log(title);
-  };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">₺{props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
