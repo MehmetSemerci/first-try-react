2226f35d6214dc2b1c6411b47b0a8be2308afcb8
Mehmet Semerci <mehmet.semerci@yahoo.com>
2021-10-09 14:39:01 +0300

    Module11PracticeStart

---
+++ module11practice/src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import mealsImage from '../../assests/meals.jpg';
+import styles from './Header.module.css';
+import HeaderCartButton from "./HeaderCartButton";
+
+
+const Header = (props) =>{
+    return(
+        <React.Fragment>
+            <header className={styles.header}>
+                <h1>ReactMeals</h1>
+                <HeaderCartButton></HeaderCartButton>
+            </header>
+            <div className={styles['main-image']}>
+                <img src={mealsImage} alt='A Table full of meals'></img>
+            </div>
+        </React.Fragment>
+    );
+};   
+
+export default Header;
