7b527c8a3f3479623b22609678ea145ef07d1162
Mehmet Semerci <mehmet.semerci@yahoo.com>
2021-10-11 22:20:41 +0300

    beforeSummary

--- Module12/src/App.js
+++ Module12/src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
+import Button from './components/UI/Button/Button';
 
 function App() {
+  const [paragraphShown, setParahraphShown] = useState(false);
+  console.log('APP');
+  const toggleParagraphShownHandler = () =>{
+    setParahraphShown((previous) => !previous);
+  };
+
+  
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={false}></DemoOutput>
+      <Button onClick={toggleParagraphShownHandler}>Show Paragraph</Button>
     </div>
   );
 }
