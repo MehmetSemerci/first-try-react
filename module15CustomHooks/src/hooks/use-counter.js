0c766c640d875631991729eb68e006a13cb5042e
Mehmet Semerci <mehmet.semerci@yahoo.com>
2022-01-06 20:54:48 +0300

    cleanup

---
+++ module15CustomHooks/src/hooks/use-counter.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+const  useCounter = (val) => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((prevCounter) => prevCounter + val);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [val]);
+
+  return counter;
+};
+
+export default useCounter;
